bd83f8d59315d02915fd13a54a95d618edc81ecd
Javier Goizueta <jgoizueta@gmail.com>
2020-09-08 13:50:34 +0200

    Cluster removed by purge

--- test/scripts/ci/install_postgres.sh
+++ test/scripts/ci/install_postgres.sh
@@ -14,7 +14,6 @@ apt-get purge postgresql* -y
 apt-get install -y --allow-unauthenticated --no-install-recommends --no-install-suggests postgresql-$POSTGRESQL_VERSION postgresql-client-$POSTGRESQL_VERSION postgresql-server-dev-$POSTGRESQL_VERSION postgresql-common
 
 # Recreate the cluster with the config we need
-pg_dropcluster --stop $POSTGRESQL_VERSION main
 rm -rf /etc/postgresql/$POSTGRESQL_VERSION /var/lib/postgresql/$POSTGRESQL_VERSION
 pg_createcluster -u postgres --locale C $POSTGRESQL_VERSION main -- -A trust
 

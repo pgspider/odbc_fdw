ecf262da8416e57286ca89354fe0743e40253e1d
Raúl Marín <git@rmr.ninja>
2020-09-08 14:48:28 +0200

    Stop the purge

--- test/scripts/ci/install_postgres.sh
+++ test/scripts/ci/install_postgres.sh
@@ -6,13 +6,10 @@ set -x
 # exit on error
 set -e
 
-# Remove all PgSQL versions
-service postgresql stop;
-apt-get purge postgresql* -y
-
 # Install the Postgresql release that we need
 apt-get install -y --allow-unauthenticated --no-install-recommends --no-install-suggests postgresql-$POSTGRESQL_VERSION postgresql-client-$POSTGRESQL_VERSION postgresql-server-dev-$POSTGRESQL_VERSION postgresql-common
 
 # Recreate the cluster with the config we need
-rm -rf /etc/postgresql/$POSTGRESQL_VERSION /var/lib/postgresql/$POSTGRESQL_VERSION
+sudo pg_dropcluster --stop $POSTGRESQL_VERSION main
+rm -rf /etc/postgresql/$POSTGRESQL_VERSION /var/lib/postgresql/$POSTGRESQL_VERSION /var/ramfs/postgresql/$POSTGRESQL_VERSION
 pg_createcluster -u postgres --locale C $POSTGRESQL_VERSION main --start -- -A trust
